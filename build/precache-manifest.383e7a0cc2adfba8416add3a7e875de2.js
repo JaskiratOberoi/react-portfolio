b03d18a6d11d37af35bff8fcd0b8fadac9f427a4
Jaskirat Singh Oberoi <Jso101196@gmail.com>
2020-03-29 10:59:06 +0530

    production build

---
+++ build/precache-manifest.383e7a0cc2adfba8416add3a7e875de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60ef7f5a2a11a676623aab4b6f540d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61f77c00db7c0fa3f6b",
+    "url": "/static/css/main.8f0e90e3.chunk.css"
+  },
+  {
+    "revision": "497afe1563c8eb274e24",
+    "url": "/static/js/2.2d367d6d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2d367d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61f77c00db7c0fa3f6b",
+    "url": "/static/js/main.13ec95bc.chunk.js"
+  },
+  {
+    "revision": "3ac27118c52e75d53249",
+    "url": "/static/js/runtime-main.4c2bc187.js"
+  }
+]);
